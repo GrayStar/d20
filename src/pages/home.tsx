66c80caafa69a58232320987579e2844b6dd4196
Adam Grayson <agrayson@xmog.com>
2022-02-04 17:04:40 -0500

    10 dice

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -9,7 +9,10 @@ import { SpotLight } from '@/components/spot-light';
 import { Die } from '@/lib/utils/models.ts';
 
 const Plane = (props: any) => {
-	const [ref] = usePlane(() => ({ rotation: [-Math.PI / 2, 0, 0], ...props }));
+	const [ref] = usePlane(() => ({
+		rotation: [-Math.PI / 2, 0, 0],
+		...props,
+	}));
 
 	return (
 		<mesh ref={ref} receiveShadow>
@@ -27,7 +30,11 @@ type IcosahedronProps = ConvexPolyhedronProps & {
 const Icosahedron = ({ size, color, ...props }: IcosahedronProps) => {
 	const geometry = useMemo(() => new THREE.IcosahedronGeometry(size, 0), [size]);
 	const args = useMemo(() => toConvexProps(geometry), [geometry]);
-	const [ref, api] = useConvexPolyhedron(() => ({ args, mass: 1, ...props }));
+	const [ref, api] = useConvexPolyhedron(() => ({
+		args,
+		mass: 1,
+		...props,
+	}));
 
 	useEffect(() => {
 		if (!props.position || !props.rotation || !props.velocity) {
@@ -36,13 +43,14 @@ const Icosahedron = ({ size, color, ...props }: IcosahedronProps) => {
 
 		api.position.set(props.position[0], props.position[1], props.position[2]);
 		api.rotation.set(props.rotation[0], props.rotation[1], props.rotation[2]);
-		api.velocity.set(props.velocity[0], props.velocity[1], props.velocity[2]);
+		api.velocity.set(0, 0, 0);
+		api.applyImpulse(props.velocity, [0, 0, 0]);
 	}, [api, api.position, api.rotation, props.position, props.rotation, props.velocity]);
 
 	return (
 		<mesh ref={ref} castShadow receiveShadow>
 			<icosahedronBufferGeometry attach="geometry" args={[size, 0]} />
-			<meshStandardMaterial attach="material" color={color} />
+			<meshPhongMaterial attach="material" color={color} />
 		</mesh>
 	);
 };
@@ -61,8 +69,6 @@ export const Home = () => {
 
 	const throwDice = () => {
 		const generatedDice = [];
-		const colors = ['red', 'purple', 'yellow', 'green', 'blue'];
-
 		for (var i = 0; i < 5; i++) {
 			const yRandom = Math.random() * 20;
 			const random = Math.random() * 5;
@@ -75,11 +81,33 @@ export const Home = () => {
 
 			const die: Die = {
 				id: i,
-				size: 1.5,
+				size: 2,
 				position: [xPosition, yPosition, zPosition],
 				velocity: [xVelocity, yVelocity, zVelocity],
 				rotation: [0, 0, 0],
-				color: colors[i],
+				color: '#88D3E7',
+			};
+
+			generatedDice.push(die);
+		}
+
+		for (var j = 0; j < 5; j++) {
+			const yRandom = Math.random() * 20;
+			const random = Math.random() * 5;
+			const xPosition = -15 - (j % 3) * 1.5;
+			const yPosition = 2 + Math.floor(j / 3) * 1.5;
+			const zPosition = -15 + (j % 3) * 1.5;
+			const xVelocity = 25 + random;
+			const yVelocity = 40 + yRandom;
+			const zVelocity = 25 + random;
+
+			const die: Die = {
+				id: j,
+				size: 2,
+				position: [-xPosition, yPosition, -zPosition],
+				velocity: [-xVelocity, yVelocity, -zVelocity],
+				rotation: [0, 0, 0],
+				color: '#ED7474',
 			};
 
 			generatedDice.push(die);
@@ -90,12 +118,12 @@ export const Home = () => {
 
 	return (
 		<div>
-			<Canvas shadows dpr={window.devicePixelRatio} style={{ width: 500, height: 500, margin: '0 auto' }}>
+			<Canvas shadows dpr={window.devicePixelRatio} style={{ width: 600, height: 600, margin: '0 auto' }}>
 				<color attach="background" args={['#F7F7F7']} />
 
 				<hemisphereLight intensity={0.35} />
 				<SpotLight
-					position={[0, 56, 0]}
+					position={[0, 56, 4]}
 					angle={0.5}
 					penumbra={1}
 					intensity={1}
@@ -104,7 +132,7 @@ export const Home = () => {
 					shadow-mapSize-height={2048}
 				/>
 
-				<Physics gravity={[0, -9.82 * 20, 0]}>
+				<Physics gravity={[0, -9.81 * 20, 0]} size={256} iterations={16}>
 					<Plane />
 
 					{dice.map((die) => {
@@ -121,7 +149,7 @@ export const Home = () => {
 					})}
 				</Physics>
 
-				<PerspectiveCamera makeDefault ref={camera} position={[0, 24, 24]} />
+				<PerspectiveCamera makeDefault ref={camera} position={[0, 48, 0]} />
 				<OrbitControls camera={camera.current} />
 				<Stats />
 			</Canvas>
