921623dbf21709c21ad0af0ed9b6a1aa5b8e66c1
Adam Grayson <agrayson@xmog.com>
2022-02-08 10:20:07 -0500

    update util names

--- src/lib/utils/three.utils.ts
+++ src/lib/utils/three.utils.ts
@@ -2,11 +2,17 @@ import type { ConvexPolyhedronProps } from '@react-three/cannon';
 import type { BufferGeometry } from 'three';
 import { Geometry } from 'three-stdlib/deprecated/Geometry';
 
-// Returns legacy geometry vertices, faces for ConvP
+// Returns legacy geometry vertices, faces for ConvexPolyhedron
 export function toConvexProps(bufferGeometry: BufferGeometry): ConvexPolyhedronProps['args'] {
-	const geo = new Geometry().fromBufferGeometry(bufferGeometry);
+	const geometry = new Geometry().fromBufferGeometry(bufferGeometry);
+
 	// Merge duplicate vertices resulting from glTF export.
 	// Cannon assumes contiguous, closed meshes to work
-	geo.mergeVertices();
-	return [geo.vertices.map((v) => [v.x, v.y, v.z]), geo.faces.map((f) => [f.a, f.b, f.c]), []];
+	geometry.mergeVertices();
+
+	return [
+		geometry.vertices.map((vertex) => [vertex.x, vertex.y, vertex.z]),
+		geometry.faces.map((face) => [face.a, face.b, face.c]),
+		[],
+	];
 }
